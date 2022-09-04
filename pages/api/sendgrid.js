51016673cdbba0ecdd7a2721af751591b2bff22f
Loïc Lécolier <loic.lecolier@outlook.be>
2022-09-04 21:50:28 +0200

    Light modifications + test SendGrid in production

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -4,6 +4,7 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
   try {
+    console.log(process.env.SENDGRID_API_KEY)
     await sendgrid.send({
       to: "contact@be-spics.com",
       from: "contact@be-spics.com",
