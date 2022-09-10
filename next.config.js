6ba5b471f26558de3545a9906f263248d462c423
Loïc Lécolier <loic.lecolier@outlook.be>
2022-09-10 12:40:32 +0200

    Redirection www

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  redirects: async () => [
+    {
+      source: '/:path*',
+      has: [{ type: 'host', value: 'www.be-spics.com' }],
+      destination: 'https://be-spics.com/:path*',
+      permanent: true
+    }
+  ]
 };
 
 module.exports = nextConfig;
