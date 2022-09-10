42667b314b16f5dd7e6fd1386dda4931aa06e34b
Loïc Lécolier <loic.lecolier@outlook.be>
2022-09-10 12:53:49 +0200

    Redirection geree par Vercel

--- next.config.js
+++ next.config.js
@@ -7,15 +7,7 @@ const nextConfig = {
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-  redirects: async () => [
-    {
-      source: '/:path*',
-      has: [{ type: 'host', value: 'www.be-spics.com' }],
-      destination: 'https://be-spics.com/:path*',
-      permanent: true
-    }
-  ]
+  }
 };
 
 module.exports = nextConfig;
