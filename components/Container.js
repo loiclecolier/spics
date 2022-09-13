84017d30c773ded7a527cbb6d077736bdf15e8f5
Loïc Lécolier <loic.lecolier@outlook.be>
2022-09-13 22:56:29 +0200

    dev to prod

--- components/Container.js
+++ components/Container.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Footer from './common/Footer'
 import Navbar from './common/Navbar'
+import { MessengerChat } from "react-messenger-chat-plugin"
 
 export default function Container({children}) {
+  
   return (
     <>
         <Navbar />
         {children}
         <Footer />
+        <MessengerChat
+        pageId="100563029485021"
+        language="fr_FR"
+      />
     </>
   )
 }
