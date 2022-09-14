8cce67340b8e446e45cb650aee7a8ba191b5fde3
Loïc Lécolier <loic.lecolier@outlook.be>
2022-09-14 22:48:01 +0200

    Mise à jour image meta tags og et twitter

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Spicx - Création de site web à Tournai et Mouscron" />
         <meta property="og:description" content="Vous êtes indépendant et vous avez besoin d'un site web ? Ne cherchez plus et faites confiance à Spicx pour vous accompagner dans votre projet !" />
-        <meta property="og:image" content="/spicx-networks.png" />
+        <meta property="og:image" content="https://spicx.be/spicx-networks.png" />
         <meta property="og:locale" content="fr_FR" />
 
         {/* Twitter Meta Tags */}
@@ -28,7 +28,7 @@ export default function Home() {
         <meta property="twitter:url" content="https://spicx.be" />
         <meta name="twitter:title" content="Spicx - Création de site web à Tournai et Mouscron" />
         <meta name="twitter:description" content="Vous êtes indépendant et vous avez besoin d'un site web ? Ne cherchez plus et faites confiance à Spicx pour vous accompagner dans votre projet !" />
-        <meta name="twitter:image" content="/spicx-networks.png" />
+        <meta name="twitter:image" content="https://spicx.be/spicx-networks.png" />
 
         {/* Add meta tag for Google Search Console */}
         <meta name="google-site-verification" content="BqNDckUJwVAFPoCFfJl59AOkTpLlsx3U-agnHJsyC1g" />
